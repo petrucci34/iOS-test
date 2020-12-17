c7a94aa81bb18f0699faffa69e57d2ad106d7a56
Korhan Bircan <korhan@squareup.com>
2020-12-16 20:14:37 -0800

    Adding test project.

---
+++ iOS-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-test
+//
+//  Created by Korhan Bircan on 12/16/20.
+//  Copyright © 2020 Korhan Bircan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
